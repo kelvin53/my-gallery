14d87c59c7398e960149206686942d449801f4e2
kelvin53 <geshkoskei53@gmail.com>
2020-08-02 18:43:27 +0300

    edit views.py

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404
+import datetime as dt
+from .models import Image,Location,Category
 
 # Create your views here.
